798dedbaca12ff479ee777fab3d30f7fe63c26d9
Joenel Sevellejo <joenelsevellejo831@gmail.com>
2026-05-13 23:07:25 +0800

    initial setup
    react vite javascript with tailwind css setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <main className="container"> 
+      <main> 
         <h1 className="text-4xl font-bold text-center mt-10 text-red-500">Welcome to my E-commerce Store</h1>
       </main>
     </>
