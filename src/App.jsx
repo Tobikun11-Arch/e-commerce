4ab77a26bb076b1f0897372ba19fbe935c967753
May-Anne Calundre <mlcalundre3m@gmail.com>
2026-05-21 08:30:01 +0800

    adding components feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Dashboard from "./components/Dashboard";
+
 export default function App() {
   return (
-    <>
-      <main> 
-        <h1 className="text-4xl font-bold text-center mt-10 text-red-500">Welcome to my E-commerce Store</h1>
-      </main>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100">
+      <Dashboard />
+    </div>
+  );
 }
