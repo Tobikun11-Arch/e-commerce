321e4a70535c366b572cf3db9b46b8d0e856c9a4
May-Anne Calundre <mlcalundre3m@gmail.com>
2026-05-22 11:06:50 +0800

    finished

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -57,7 +57,7 @@ export default function ProductCard() {
   return (
     <div className="p-6">
       <h2 className="text-xl font-bold text-gray-800 mb-6">
-        Top Selling Products
+        T-Shirt Collections
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
